56fb143255fe9f11c70a3009c98326e28043a226
ljubica <apple@BEG-Krstic-Ljubica.local>
2021-08-03 16:03:25 +0200

    input component started

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+
+import { ReactComponent as Magnifier } from '../../assets/svg/magnifier.svg';
+import { ReactComponent as Location } from '../../assets/svg/location.svg';
 
 import './Input.scss';
 
-const Input = ({ size = 'medium', type = 'text', ...rest }) => {
-  return <input type={type} className={`input input--${size}`} {...rest} />;
+const Input = ({ variant = 'light', type = 'location', ...rest }) => {
+  const adjustInputSize = () => {
+    const inputField = document.querySelector('.input__field');
+    inputField.setAttribute(
+      'size',
+      inputField.getAttribute('placeholder').length,
+    );
+  };
+
+  useEffect(() => {
+    adjustInputSize();
+  }, []);
+
+  return (
+    <div className={`input input--${variant}`}>
+      <div className='input__icon-wrapper'>
+        {type === 'location' ? (
+          <Location className='input__icon' />
+        ) : (
+          <Magnifier className='input__icon' />
+        )}
+      </div>
+      <input {...rest} className='input__field' />
+    </div>
+  );
 };
 
 Input.propTypes = {
-  size: PropTypes.string,
+  variant: PropTypes.string,
   type: PropTypes.string,
 };
 
