81546e13fabb972ddf20fca4e413c716b05c2d11
ljubica <apple@BEG-Krstic-Ljubica.local>
2021-08-18 16:10:09 +0200

    Mobile search

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,31 +1,24 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { ReactComponent as Magnifier } from '../../assets/svg/magnifier.svg';
 import { ReactComponent as Location } from '../../assets/svg/location.svg';
+import { ReactComponent as Filter } from '../../assets/svg/filter-icon.svg';
 
 import './Input.scss';
 
 const Input = ({ variant = 'light', type = 'location', ...rest }) => {
-  const adjustInputSize = () => {
-    const inputField = document.querySelector('.input__field');
-    inputField.setAttribute(
-      'size',
-      inputField.getAttribute('placeholder').length,
-    );
-  };
-
-  useEffect(() => {
-    adjustInputSize();
-  }, []);
-
   return (
-    <div className={`input input--${variant}`}>
+    <div className={`input input--${variant} input--${type}`}>
       <div className='input__icon-wrapper'>
         {type === 'location' ? (
           <Location className='input__icon' />
-        ) : (
+        ) : type === 'magnifier' ? (
           <Magnifier className='input__icon' />
+        ) : (
+          <button className='input__filter-button'>
+            <Filter className='icon-input' />
+          </button>
         )}
       </div>
       <input {...rest} className='input__field' />
