2b2476c8abd66dcfe4d0db5f2c0b25c16f00eb38
ljubica <apple@BEG-Krstic-Ljubica.local>
2021-08-18 17:06:06 +0200

    dashboard started

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ import { ReactComponent as MiddleDot } from '../../assets/svg/middle-dot.svg';
 
 import './Card.scss';
 
-const Card = ({ cardInfo, variant }) => {
+const Card = ({ cardInfo, variant = 'light' }) => {
   return (
     <div className={`card card--${variant}`}>
       <img
@@ -14,15 +14,17 @@ const Card = ({ cardInfo, variant }) => {
       />
       <div className='card__content'>
         <div className='card__info'>
-          <span className='card__time'>{cardInfo.time}</span>
+          <span className='card__time'>
+            {cardInfo.numberOfQuestions} questions
+          </span>
           <MiddleDot className='card__middle-dot' />
-          <span className='card__engagement'>{cardInfo.engagement}</span>
+          <span className='card__engagement'>{cardInfo.level} level</span>
         </div>
         <a href='https://resources.workable.com/senior-software-engineer-job-description'>
-          <h3 className='card__title'>{cardInfo.title}</h3>
+          <h3 className='card__title'>{cardInfo.name}</h3>
         </a>
-        <span className='card__company'>{cardInfo.company}</span>
-        <span className='card__location'>{cardInfo.location}</span>
+        <span className='card__company'>{cardInfo.description}</span>
+        <span className='card__location'>Duration: {cardInfo.duration}</span>
       </div>
     </div>
   );
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../../constants/routes';
+import firebase from 'firebase';
+
+import Header from '../../components/Header';
+import Card from '../../components/Card';
 
 import './Dashboard.scss';
 
 const Dashboard = () => {
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection('tests')
+      .get()
+      .then((snapshot) => {
+        const testsArray = snapshot.docs.map((doc) => doc.data());
+        console.log(testsArray);
+        setTests(testsArray);
+      });
+  }, []);
   return (
     <div className='dashboard'>
       DASHBOARD
-      <Link to={`${routes.TEST}/Test1`}>Test1</Link>
-      <Link to={`${routes.TEST}/Test2`}>Test2</Link>
+      {tests && tests.map((test) => <Card key={test.name} cardInfo={test} />)}
     </div>
   );
 };
